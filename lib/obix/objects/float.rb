107b16dc6352c19769e92f71ad4d69157a1a20a7
Johannes Gorset <jgorset@gmail.com>
2013-01-03 14:20:37 +0100

    Default float objects to 0.0

--- lib/obix/objects/float.rb
+++ lib/obix/objects/float.rb
@@ -3,7 +3,7 @@ module OBIX
 
     # Floats represent floating point numbers.
     class Float < Object
-      attribute :val, type: Types::Float
+      attribute :val, type: Types::Float, default: 0.0
       attribute :min, type: Types::Float
       attribute :max, type: Types::Float
       attribute :unit, type: Types::String
