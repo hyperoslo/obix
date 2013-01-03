fc272f9e14d0052e17078e2b25aa719049f417d3
Johannes Gorset <jgorset@gmail.com>
2013-01-03 14:19:42 +0100

    Facilitate for default attribute values

--- lib/obix/accessible.rb
+++ lib/obix/accessible.rb
@@ -7,10 +7,11 @@ module OBIX
     # options - A Hash of query options:
     #           :type - A class describing the type of this attribute (defaults to nil).
     def attribute name, options = {}
-      type = options.fetch :type, nil
+      type    = options.fetch :type, nil
+      default = options.fetch :default, nil
 
       define_method name do
-        value = @attributes[name.to_s]
+        value = @attributes[name.to_s] || default
 
         if type
           type.cast value
---
+++ test/accessible_test.rb
@@ -0,0 +1,62 @@
+require "obix"
+require "minitest/unit"
+require "minitest/autorun"
+require "nokogiri"
+require "active_support/all"
+
+require "test_helper"
+
+class AccessibleTest < MiniTest::Unit::TestCase
+
+  class Dummy
+    include OBIX
+
+    extend Accessible
+
+    attribute :string, type: Types::String
+    attribute :integer, type: Types::Integer
+    attribute :float, type: Types::Float
+    attribute :boolean, type: Types::Boolean
+    attribute :default, default: "default"
+
+    def initialize attributes = {}
+      @attributes = attributes
+    end
+  end
+
+  def test_creates_access_method
+    test = Dummy.new "string" => "string"
+
+    assert test.string
+  end
+
+  def test_casts_strings
+    test = Dummy.new "string" => "string"
+
+    assert_equal "string", test.string
+  end
+
+  def test_casts_integers
+    test = Dummy.new "integer" => "1"
+
+    assert_equal 1, test.integer
+  end
+
+  def test_casts_floats
+    test = Dummy.new "float" => "1.0"
+
+    assert_equal 1.0, test.float
+  end
+
+  def test_casts_booleans
+    test = Dummy.new "boolean" => "true"
+
+    assert_equal true, test.boolean
+  end
+
+  def test_defaults
+    test = Dummy.new
+
+    assert_equal "default", test.default
+  end
+end
