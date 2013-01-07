9e54b83ce3d27252fa523abbdb20122d3b9f6ee1
Johannes Gorset <jgorset@gmail.com>
2013-01-07 10:08:26 +0100

    Add tests for floats

---
+++ test/objects/float_test.rb
@@ -0,0 +1,16 @@
+require "obix"
+require "minitest/unit"
+require "minitest/autorun"
+
+require "test_helper"
+
+class FloatTest < MiniTest::Unit::TestCase
+
+  def test_parses_floats
+    object = OBIX.parse fixture "objects/float.xml"
+
+    assert_instance_of OBIX::Objects::Float, object
+    assert_equal 41.06, object.val
+  end
+
+end
