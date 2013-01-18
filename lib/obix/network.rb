e2313845a48e2a5fe1a844f03a5a874ebfbe9687
Johannes Gorset <jgorset@gmail.com>
2013-01-18 11:33:37 +0100

    Implement OBIX::Network

---
+++ lib/obix/network.rb
@@ -0,0 +1,61 @@
+require "net/http"
+
+module OBIX
+  module Network
+    include Net
+
+    extend self
+
+    # Make a get request.
+    #
+    # url    - A String describing where the request should be sent.
+    # object - A Objects::Object or derivative thereof describing the OBIX element to send.
+    def get url
+      url     = URI url
+      request = HTTP::Get.new url.path
+
+      dispatch request, to: url
+    end
+
+    # Make a post request.
+    #
+    # url    - A String describing where the request should be sent.
+    # object - A Objects::Object or derivative thereof describing the OBIX element to send.
+    def post url, object = nil
+      url     = URI url
+      request = HTTP::Post.new url.path
+
+      if object
+        request.body = object.to_xml
+      end
+
+      dispatch request, to: url
+    end
+
+    private
+
+    # Dispatch the given request.
+    #
+    # request - A derivative of Net::HTTPGenericRequest describing the request.
+    # options - A Hash with query options:
+    #           :to - An URI instance describing where the request should be sent.
+    #
+    # Returns a Net::HTTPResponse instance.
+    def dispatch request, options
+      url = options.fetch :to
+
+      response = HTTP.start url.host, url.port do |http|
+        request.basic_auth OBIX.configuration.username, OBIX.configuration.password
+        http.request request
+      end
+
+      unless response.code == 200
+        raise Error, "The server responded with an unexpected status code #{response.code} for #{url}."
+      end
+
+      response.body
+    end
+
+    class Error < StandardError; end
+  end
+end
---
+++ test/network_test.rb
@@ -0,0 +1,85 @@
+require "obix"
+require "minitest/unit"
+require "minitest/autorun"
+require "active_support/all"
+
+require "test_helper"
+
+class NetworkTest < MiniTest::Unit::TestCase
+  include Net
+
+  def test_get
+    HTTP.
+    expects(
+      :start
+    ).
+    with(
+      "example.org", 80
+    ).
+    returns(
+      stub code: 200, body: "<body>"
+    )
+
+    body = OBIX::Network.get "http://example.org/"
+
+    assert_equal "<body>", body
+  end
+
+  def test_post
+    HTTP.
+    expects(
+      :start
+    ).
+    with(
+      "example.org", 80
+    ).
+    returns(
+      stub code: 200, body: "<body>"
+    )
+
+    body = OBIX::Network.post "http://example.org/"
+
+    assert_equal "<body>", body
+  end
+
+  def test_post_with_object
+    HTTP.
+    expects(
+      :start
+    ).
+    with(
+      "example.org", 80
+    ).
+    returns(
+      stub code: 200, body: "<body>"
+    )
+
+    object = mock
+
+    object.
+    expects(
+      :to_xml
+    ).
+    returns(
+      fixture "objects/string.xml"
+    )
+
+    body = OBIX::Network.post "http://example.org/", object
+
+    assert_equal "<body>", body
+  end
+
+  def test_bad_response
+    HTTP.
+    expects(
+      :start
+    ).
+    returns(
+      stub code: 500, body: nil
+    )
+
+    assert_raises OBIX::Network::Error do
+      OBIX::Network.get "http://example.org/"
+    end
+  end
+end
