f944ef31daaa1bb3341ad7893164d7dc213d66cb
Johannes Gorset <jgorset@gmail.com>
2013-01-25 15:12:25 +0100

    Remove unused fixture

--- test/builder_test.rb
+++ test/builder_test.rb
@@ -8,8 +8,6 @@ require "test_helper"
 
 class BuilderTest < MiniTest::Unit::TestCase
   def test_builds
-    xml = fixture "valid.xml"
-
     obix = OBIX::Builder.new do |obix|
       obix.obj name: "foo", href: "bar" do |obix|
         obix.obj name: "bar" do |obix|
