78e53ec86f77e8da1d294fdef616f85c444c6c60
Johannes Gorset <jgorset@gmail.com>
2014-02-12 14:02:05 +0100

    Improve documentation

--- lib/obix/types/duration.rb
+++ lib/obix/types/duration.rb
@@ -7,7 +7,7 @@ module OBIX
 
       # Cast the given value to an integer describing seconds.
       #
-      # value - A String describing an XSD
+      # value - A String describing an XSD (http://www.w3schools.com/schema/schema_dtypes_date.asp).
       #
       # Returns an Integer.
       def cast value
